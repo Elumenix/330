974668c9b367b42cf0e51b38f446fbfa6fb22f34
Elumenix <89927445+Elumenix@users.noreply.github.com>
2023-01-21 16:27:29 -0500

    Finished PE01

---
+++ PE's/scullion-d-pe01/src/main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
+
+const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
+
+const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+
+// References
+let output;
+let button;
+
+// Initialize References
+window.onload = function () {
+    output = document.querySelector("#output");
+    button = document.querySelector("#myButton");
+
+    // Event
+    button.onclick = outputWords;
+}
+
+
+function outputWords() {
+
+    // Helper Method
+    function grabArrayMember(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
+
+    // Random word is grabbed from each array and output
+    output.innerHTML = `${grabArrayMember(words1)} ${grabArrayMember(words2)} ${grabArrayMember(words3)}`;
+}
