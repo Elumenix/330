6e5343cc2a789e70dc3dc73465025f9df68bead3
Elumenix <89927445+Elumenix@users.noreply.github.com>
2023-02-22 19:04:00 -0500

    Turned all of audio.js into arrow functions
    Also refactored all the code

--- PE's/scullion-d-pe06/web-audio-hw/src/audio.js
+++ PE's/scullion-d-pe06/web-audio-hw/src/audio.js
@@ -7,71 +7,71 @@ let element, sourceNode, analyserNode, gainNode;
 
 // 3 - here we are faking an enumeration
 const DEFAULTS = Object.freeze({
-    gain       :       .5,
-    numSamples :       256
+    gain: .5,
+    numSamples: 256
 });
 
 // 4 - create a new array of 8-bit integers (0-255)
 // this is a typed array to hold the audio frequency data
-let audioData = new Uint8Array( DEFAULTS.numSamples/2);
+let audioData = new Uint8Array(DEFAULTS.numSamples / 2);
 
 // **Next are "public" methods - we are going to export all of these at the bottom of this file**
-function setupWebaudio(filePath){
-// 1 - The || is because WebAudio has not been standardized across browsers yet
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-audioCtx = new AudioContext();
+const setupWebaudio = (filePath) => {
+    // 1 - The || is because WebAudio has not been standardized across browsers yet
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    audioCtx = new AudioContext();
 
-// 2 - this creates an <audio> element
-element = new Audio(); //document.querySelector("audio");
+    // 2 - this creates an <audio> element
+    element = new Audio(); //document.querySelector("audio");
 
-// 3 - have it point at a sound file
-loadSoundFile(filePath);
+    // 3 - have it point at a sound file
+    loadSoundFile(filePath);
 
-// 4 - create an a source node that points at the <audio> element
-sourceNode = audioCtx.createMediaElementSource(element);
+    // 4 - create an a source node that points at the <audio> element
+    sourceNode = audioCtx.createMediaElementSource(element);
 
-// 5 - create an analyser node
-// note the UK spelling of "Analyser"
-analyserNode = audioCtx.createAnalyser();
+    // 5 - create an analyser node
+    // note the UK spelling of "Analyser"
+    analyserNode = audioCtx.createAnalyser();
 
-/*
-// 6
-We will request DEFAULTS.numSamples number of samples or "bins" spaced equally 
-across the sound spectrum.
+    /*
+    // 6
+    We will request DEFAULTS.numSamples number of samples or "bins" spaced equally 
+    across the sound spectrum.
+    
+    If DEFAULTS.numSamples (fftSize) is 256, then the first bin is 0 Hz, the second is 172 Hz, 
+    the third is 344Hz, and so on. Each bin contains a number between 0-255 representing 
+    the amplitude of that frequency.
+    */
 
-If DEFAULTS.numSamples (fftSize) is 256, then the first bin is 0 Hz, the second is 172 Hz, 
-the third is 344Hz, and so on. Each bin contains a number between 0-255 representing 
-the amplitude of that frequency.
-*/ 
+    // fft stands for Fast Fourier Transform
+    analyserNode.fftSize = DEFAULTS.numSamples;
 
-// fft stands for Fast Fourier Transform
-analyserNode.fftSize = DEFAULTS.numSamples;
+    // 7 - create a gain (volume) node
+    gainNode = audioCtx.createGain();
+    gainNode.gain.value = DEFAULTS.gain;
 
-// 7 - create a gain (volume) node
-gainNode = audioCtx.createGain();
-gainNode.gain.value = DEFAULTS.gain;
-
-// 8 - connect the nodes - we now have an audio graph
-sourceNode.connect(analyserNode);
-analyserNode.connect(gainNode);
-gainNode.connect(audioCtx.destination);
+    // 8 - connect the nodes - we now have an audio graph
+    sourceNode.connect(analyserNode);
+    analyserNode.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
 }
 
-function loadSoundFile(filePath){
+const loadSoundFile = (filePath) => {
     element.src = filePath;
 }
 
-function playCurrentSound(){
+const playCurrentSound = () => {
     element.play();
 }
 
-function pauseCurrentSound(){
+const pauseCurrentSound = () => {
     element.pause();
 }
 
-function setVolume(value) {
+const setVolume = (value) => {
     value = Number(value); // make sure that it's a Number rather than a String
     gainNode.gain.value = value;
 }
 
-export {audioCtx,setupWebaudio,playCurrentSound,pauseCurrentSound,loadSoundFile,setVolume,analyserNode};
+export { audioCtx, setupWebaudio, playCurrentSound, pauseCurrentSound, loadSoundFile, setVolume, analyserNode };
