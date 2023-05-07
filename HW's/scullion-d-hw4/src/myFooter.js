d578b335be229718b5a6a71c4a5555d9b50baee7
Elumenix <89927445+Elumenix@users.noreply.github.com>
2023-05-07 19:57:05 -0400

    Slots seem to be working, more testing might be needed

---
+++ HW's/scullion-d-hw4/src/myFooter.js
@@ -0,0 +1,40 @@
+const temp = document.createElement("template");
+temp.innerHTML = `
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link href="https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="styles/default-styles.css">
+<div class="footer has-background-info has-text-centered has-text-light p-1">
+    &copy; <slot name="year"></slot> <slot name="title"></slot>
+</div>
+`;
+
+class myFooter extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(temp.content.cloneNode(true));
+    }
+
+    static get observedAttributes() {
+        return ["data-title", "data-year"];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.render();
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        const titleSlot = this.shadowRoot.querySelector('slot[name="title"]');
+        const yearSlot = this.shadowRoot.querySelector('slot[name="year"]');
+        titleSlot.textContent = this.dataset.title || "No title provided";
+        yearSlot.textContent = this.dataset.year || "No subtitle provided";
+    }
+
+}
+
+customElements.define("my-footer", myFooter);
---
+++ HW's/scullion-d-hw4/src/myHeader.js
@@ -0,0 +1,44 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link href="https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="styles/default-styles.css">
+<header class="hero is-small is-info is-bold p-2">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title"><slot name="title">No title provided</slot></h1>
+      <h2 class="subtitle"><slot name="subtitle">No subtitle provided</slot></h2>
+    </div>
+  </div>
+</header>
+`;
+
+class MyHeader extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    static get observedAttributes() {
+        return ["data-title", "data-subtitle"];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.render();
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        const titleSlot = this.shadowRoot.querySelector('slot[name="title"]');
+        const subtitleSlot = this.shadowRoot.querySelector('slot[name="subtitle"]');
+        titleSlot.textContent = this.dataset.title || "No title provided";
+        subtitleSlot.textContent = this.dataset.subtitle || "No subtitle provided";
+    }
+}
+
+customElements.define("app-header", MyHeader);
